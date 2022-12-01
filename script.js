b4109d03796768a4ed27778310599ee31dab89b7
Shaka Suzy <shaheedchambers@gmail.com>
2022-11-30 20:57:15 -0500

    structed retrun on button

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 // Assignment Code
+//needs global variables
 var generateBtn = document.querySelector("#generate");
+
+// adding function generatePassword(){
+function generatePassword(){
+  console.log("Button totally pressed");
+
+  return "Generated password is here"
+}
+//}
+//wheres the values?
 
 // Write password to the #password input
 function writePassword() {
@@ -7,7 +17,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+//here
 }
 
 // Add event listener to generate button
